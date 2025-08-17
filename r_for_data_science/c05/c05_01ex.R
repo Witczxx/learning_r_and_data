4b7b51497224c3a91e265cad18675b5aebbee304
Patrick Witczak <patrick.witczak@web.de>
2025-08-17 18:07:30 +0800

    completed exercises, where I had to do exploratory data analysis. Exercises mainly required usage of geom_histogram() to analyse numerical variables ['R for Data Science' (Wickham, Grolemund)]

--- r_for_data_science/c05/c05_01ex.R
+++ r_for_data_science/c05/c05_01ex.R
@@ -1,3 +1,77 @@
 # Exercises from the Book "R For Data Science" (Wickham, Grolemund)
 # Chapter 05 - Notes 01
-# #
+# #ExploratoryDataAnalysis #EDA #Variable #Value #Observation #TabularData #Variation #VisualisingDistributions
+# #Subgroups #UntypicalValues #Outliers
+
+library(tidyverse)
+data(diamonds)
+
+# EDA = Exploratory Data Analysis
+# decsribed as an iterative cycle
+
+# Includes following steps:
+# 1. Generate questions about your data. 
+# 2. Search for answers by visualizing, transforming, and modeling your data. 
+# 3. Use what you learn to refine your questions and/or generate new questions.
+
+# Two major types of questions (loosely said):
+# 1. What type of variation occurs within my variables? 
+# 2. What type of covariation occurs between my variables?
+
+# Definitions
+# A variable is a quantity, quality, or property that you can meas‐ ure.
+# A value is the state of a variable when you measure it. 
+#   (The value of a variable may change from measurement to measurement)
+# An observation (st data point), or a case, is a set of measurements made under similar conditions
+# Tabular data is a set of values, each associated with a variable and an observation.
+
+# Variation
+# Variation is the tendency of the values of a variable to change from measurement to measurement.
+# Categorical variables can also vary if you measure across different subjects.
+# The best way to understand that pattern is to visualize the distribution of variables’ values.
+
+# Visualising Distributions
+# How you visualise depends on whether the variable is categorical or continuous.
+# Distribution of a categorical variable (finite), use a bar chart.
+# Distribution of a continuous variable (infinite), use a histogram.
+#   Overlay multiple histograms -> use geom_freqpoly() instead of geom_histogram().
+#   Instead of displaying bars, uses lines. It is easier to understand.
+
+# Typical Values
+# To turn this information into useful questions, look for anything unexpected:
+#   Which values are the most common? Why? 
+#   Which values are rare? Why? 
+#   Does that match your expectations? 
+#   Can you see any unusual patterns? 
+#   What might explain them?
+
+# Subgroups
+# In general, clusters of similar values suggest that subgroups exist in your data.
+# To understand subgroups, ask:
+#   How are the observations within each cluster similar to each other? 
+#   How are the observations in separate clusters different from each other? 
+#   How can you explain or describe the clusters? 
+#   Why might the appearance of clusters be misleading?
+# Many of the preceding questions will prompt you to explore a relationship between variables,
+
+# Untypical Values
+# Unusual Values Outliers are observations that are unusual; data points that don’t seem to fit the pattern. 
+# Sometimes outliers are data entry errors; other times outliers suggest important new science.
+# To make it easy to see the unusual values, we need to zoom in to small values of the y-axis with cord_cartesian()
+# Let's look at an example: 
+ggplot(diamonds) + 
+  geom_histogram(mapping = aes(x = y), binwidth = 0.5) + 
+  coord_cartesian(ylim = c(0, 50)) # xlim= argument also exists, if we need to zoom into the x-axis
+# Lets filter the outliers to take a closer look:
+unusual <-diamonds %>% filter(y < 3 | y > 20) %>% arrange(y)
+# We know that diamonds can’t have a width of 0mm, so these values must be incorrect. 
+# We might also suspect that meas‐ urements of 32mm and 59mm are implausible
+
+# What to do with outliers
+# If they have minimal effect on the results, 
+#   and you can’t figure out why they’re there, 
+#   it’s reasonable to replace them with missing values and move on.
+# If they have a substantial effect on your results, 
+#   you shouldn’t drop them without justification. 
+#   You’ll need to figure out what caused them (e.g., a data entry error) 
+#   and disclose that you removed them in your write-up.
--- r_for_data_science/c05/c05_01nt.R
+++ r_for_data_science/c05/c05_01nt.R
@@ -1,3 +1,65 @@
 # Exercises from the Book "R For Data Science" (Wickham, Grolemund)
 # Chapter 05 - Exercise 01
-# #
+# #ExploratoryDataAnalysis #EDA #Variable #Value #Observation #TabularData #Variation #VisualisingDistributions
+# #Subgroups #UntypicalValues #Outliers
+
+# 1. Explore the distribution of each of the x, y, and z variables in diamonds. 
+#    What do you learn? 
+#    Think about a diamond and how you might decide which dimension is the length, width, and depth. 
+### We have some outliers, but using geom_smooth() clearly show that the values of x and y are quite equal in each observation.
+### While the x/y and z are tending to have a proportion of z = x * 2/3, with a slight decrease at high weights. 
+### There are a view outliers, which most likely are errors. For each parameter they are less than 5 observations with untypical values.
+summary(select(diamonds, x, y, z))
+ggplot(diamonds) + geom_histogram(mapping = aes(x = x), binwidth = 0.01)
+ggplot(diamonds) + geom_histogram(mapping = aes(x = y), binwidth = 0.01)
+ggplot(diamonds) + geom_histogram(mapping = aes(x = z), binwidth = 0.01)
+# Removing outliers
+filter(diamonds, x > 0, x < 10) %>% ggplot() + geom_histogram(mapping = aes(x = x), binwidth = 0.01) + scale_x_continuous(breaks = 1:10)
+filter(diamonds, y > 0, y < 10) %>% ggplot() + geom_histogram(mapping = aes(x = y), binwidth = 0.01) + scale_x_continuous(breaks = 1:10)  
+filter(diamonds, z > 0, z < 10) %>% ggplot() + geom_histogram(mapping = aes(x = z), binwidth = 0.01) + scale_x_continuous(breaks = 1:10)    
+# Z is less than both x and y 
+summarise(diamonds, mean(x > y), mean(x > z), mean(y > z))
+
+# 2. Explore the distribution of price. 
+#    Do you discover anything unusual or surprising? 
+#   (Hint: carefully think about the bin width and make sure you try a wide range of values.) 
+### In the lower pricing-area we have a very high count, wiht a peak at around 400 of around 1500 diamonds.
+### From that moment on, the count is dropping with a rising price.
+### There is a small increase in the area around 4000. People seem to prefer staying under thresholds like 500 and 5000. 
+### From a price of 10000, the total count in a span of 1000 is under 1000, reaching its lowest point of 300 at 18000.
+?diamonds
+summary(select(diamonds, price))
+ggplot(diamonds) + geom_histogram(mapping = aes(x = price), binwidth = 100)
+filter(diamonds, price < 2500) |> 
+ggplot() + geom_histogram(mapping = aes(x = price), binwidth = 50)
+filter(diamonds, price > 10000) |> 
+ggplot() + geom_histogram(mapping = aes(x = price), binwidth = 10)
+diamonds |> 
+  mutate(round_price = price %/% 1000) |> 
+  group_by(round_price) |> 
+  summarise(n = n())
+
+# 3. How many diamonds are 0.99 carat? 
+#    How many are 1 carat? 
+#    What do you think is the cause of the difference? 
+### There are only 23 diamonds with 0.99 carat, but 1558 diamonds with 1 carat
+### It is easier to sell diamonds with 1 carat. Filter in online-shops or requests of customers.
+View(filter(diamonds, between(carat, 0.99, 1)))
+diamonds |> 
+  filter(carat == 0.99 | carat == 1) |> 
+  group_by(carat) |> 
+  summarise(carat_count = n())
+
+# 4. Compare and contrast coord_cartesian() versus xlim() or ylim() when zooming in on a histogram. 
+#    What happens if you leave binwidth unset? 
+#    What happens if you try and zoom so only half a bar shows?
+### coord_cartesian() does really a visual zoom. The look of the data stays unchanged.
+### xlim and ylim drop all values outside the limit as NA, influencing how the visualisation looks.
+?coord_cartesian
+ggplot(diamonds) +
+  geom_histogram(mapping = aes(x = price)) +
+  coord_cartesian(xlim = c(100, 5000), ylim = c(0, 3000))
+ggplot(diamonds) +
+  geom_histogram(mapping = aes(x = price)) +
+  xlim(100, 5000) +
+  ylim(0, 3000)
